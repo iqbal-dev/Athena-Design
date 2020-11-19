789384c8cad4105beb4f704627e906310bf93549
bashir ahammed <bahammed16@gmail.com>
2020-11-19 08:05:12 +0400

    midddle section done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Achivement from './components/OurAchivements/Achivement';
+import StaRunning from './components/StayRunning/StayRunning'
+
+function App() {
+  return (
+    <>
+     <StaRunning />
+     <Achivement />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StayRunning/StayRunning.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
+import man from '../../images/man.png';
+import './StayRunning.css'
+const StayRunning = () => {
+    return (
+        <Container className='mb-5'>
+          <Row className='d-flex justify-content-between align-items-center '>
+              <Col    md={6}>
+                 <Image className='w-100' src={man}  alt=''   />
+              </Col>
+              <Col  md={6} sm={12}  xl={6}>
+                 <div className='ml-5'>
+                     <h3>Stay Running & Project</h3>
+                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit
+                      Incidunt accusantium omnis obcaecati quas deserunt
+                      illo autem quis, nihil asperiores dolorum .</p>
+                    <div className='mt-5'>
+                       <div className='button-contact'>Contact us</div> 
+                    </div>
+                 </div>
+              </Col>
+          </Row>
+        </Container>
+    );
+};
+
+export default StayRunning;
